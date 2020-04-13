0623758b7bc6e1ef914a12fa937944a92116c7fb
Yashwanth <yashwanth@pop-os.localdomain>
2020-04-13 19:36:13 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const connectDB = require("./config/db");
 connectDB();
@@ -14,5 +15,15 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/tests", require("./routes/api/tests"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/feeds", require("./routes/api/feeds"));
+
+// Serving static assets
+if (process.env.NODE_ENV === "production") {
+  // setting static foler
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
